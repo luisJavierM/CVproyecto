cc3e17357b3da7d955b64c9e5b39b7ce3de243a3
luisJavierM <126623865+luisJavierM@users.noreply.github.com>
2023-02-28 19:20:35 -0300

    nuevo
    nuevo

---
+++ js/CV.js
@@ -0,0 +1,8 @@
+document.getElementById("boton_default").addEventListener('click', function(){
+    document.body.style.backgroundColor = 'white';
+    
+    if (document.body.style.backgroundColor === 'white') {
+        document.body.style.backgroundColor = 'black';
+        
+    }
+});
