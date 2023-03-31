c68c71edac1a2685eba247c9adf4ea3ca7f3ef41
luisJavierM <126623865+luisJavierM@users.noreply.github.com>
2023-03-31 00:09:16 -0300

    Update CV.js

--- js/CV.js
+++ js/CV.js
@@ -1,8 +1,17 @@
-document.getElementById("boton_default").addEventListener('click', function(){
-    document.body.style.backgroundColor = 'white';
-    
-    if (document.body.style.backgroundColor === 'white') {
-        document.body.style.backgroundColor = 'black';
-        
+const $generate = document.getElementById('boton_default');
+
+let hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+document.addEventListener('click', e => {
+    if (e.target === $generate) {
+        let color = "#";
+        for (let i = 0; i < 6; i++) color += hex[randomNumber()];
+        document.body.style.background = color;
     }
-});
+
+})
+
+function randomNumber() {
+    return Math.floor(Math.random() * hex.length)
+}
